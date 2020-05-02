d92bfa9c8be2e325a568f4384a40abdb486b4555
Vladyslav Parashchenko <vladyslavparashchenko37@gmail.com>
2020-05-02 18:39:27 +0300

    refactor: amqp service

--- app/Services/AMPQ.js
+++ app/Services/AMPQ.js
@@ -7,20 +7,15 @@ function instantiateService (serviceConfig = {}) {
 
   return {
     async listenQueue (queueName, callback) {
-      return amqplib.connect(connectionUrl)
-        .then(function (conn) {
-          return conn.createChannel()
-        })
-        .then(function (ch) {
-          return ch.assertQueue(queueName).then(function (ok) {
-            return ch.consume(queueName, function (msg) {
-              if (msg !== null) {
-                callback(JSON.parse(msg.content.toString()))
-                ch.ack(msg)
-              }
-            })
-          })
-        })
+      const conn = await amqplib.connect(connectionUrl)
+      const ch = await conn.createChannel()
+      await ch.assertQueue(queueName)
+      return ch.consume(queueName, function (msg) {
+        if (msg !== null) {
+          callback(JSON.parse(msg.content.toString()))
+          ch.ack(msg)
+        }
+      })
     },
 
     async buildMessageSender (queueName) {
